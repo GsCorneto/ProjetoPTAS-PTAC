5179cc987d0e6404477c538ae08e8a32631cb7be
GsCorneto <guilherme.corneto@gmail.com>
2024-10-30 22:29:45 -0400

    ADD: check-itens
    Uma forma de verificação dos objetos fornecidos foi adicionada, checagem de email, nome e senha através do tamanho da string

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -11,8 +11,30 @@ class AuthController{
     }
 
     static async cadastro(req,res){
-        
+        const{nome, email, password} = req.body;
 
+        if(!nome || nome.lenght < 6){
+            return res.json({
+                erro: true,
+                mensagem: "O nome deve conter mais de 6 caracteres."
+            })}
+
+        if(!email || email.lenght < 10){
+            return res.json({
+                erro: true,
+                mensagem: "Email inválido."
+            })}
+
+        if(!password || password.lenght < 8){
+            return res.json({
+                erro: true,
+                mensagem: "A senha deve possuir mais de 8 caracteres."
+            })}
+
+            return res.json({
+                erro: false,
+                mensagem: "Cadastro efetuado com sucesso!"
+            })
     }
 }
 
