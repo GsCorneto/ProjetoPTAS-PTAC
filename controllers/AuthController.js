795f7fb423a3efb7d8ee273818add6c0b3c4a9c6
GsCorneto <guilherme.corneto@gmail.com>
2024-10-30 23:00:28 -0400

    ADD: security
    Adicionada feature de verificar se o usuário já está cadastrado utilizando .count. Ao instalar o pacote bcrypt, é possivel criar o hash das senhas dos usuarios (hash = senha criptografada)

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,8 @@
 const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient();
 
+const bcrypt = require("bcryptjs"); 
+
 class AuthController{
     static async login(req,res){
         res.json({
@@ -31,10 +33,46 @@ class AuthController{
                 mensagem: "A senha deve possuir mais de 8 caracteres."
             })}
 
-            return res.json({
-                erro: false,
-                mensagem: "Cadastro efetuado com sucesso!"
+            const existUser = await prisma.usuario.count({
+                where: {
+                    email : email,  
+                }
             })
+
+            if(existUser != 0){
+                return res.json({
+                    erro: true,
+                    mensagem: "Já existe um usuário cadastrado com este e-mail."
+                })
+            }
+
+        const salt = bcrypt.genSaltSync(4);
+        const hashPassword = bcrypt.hashSync(password, salt);
+
+            try{
+                const usuario = await prisma.usuario.create({
+                    data: {
+                        nome: nome,
+                        email: email,
+                        password: hashPassword,
+                        tipo: "cliente",
+                    }
+                });
+
+                return res.json({
+                    erro: false,
+                    mensagem: "Cadastro efetuado com sucesso!"
+                })
+
+            } catch (error){
+                return res.json({
+                    erro: true,
+                    mensagem: "Erro! " + error
+                })
+            }
+            
+
+            
     }
 }
 
